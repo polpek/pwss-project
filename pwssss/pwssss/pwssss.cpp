58375c967559a02c83bc23d586317810a1d38e75
polpek <115738250+polpek@users.noreply.github.com>
2023-01-09 18:55:50 +0100

    Add files via upload

---
+++ pwssss/pwssss/pwssss.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#pragma comment(lib, "ws2_32.lib")
+
+
+#define MAX_CONNECTIONS 10
+#define MAX_BUFFER_SIZE 1024
+
+
+const int MAX_CLIENTS = 10;
+const int BUFFER_SIZE = 4096;
+
+
+
+// tworzenie gniazda
+int create_socket(int port)
+{
+    int sockfd;
+    sockaddr_in serv_addr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        std::cerr << "Blad tworzenia gniazda" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    int opt = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+    {
+        std::cerr << "Blad w setsockopt" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(port);
+
+    if (bind(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        std::cerr << "Blad w bind" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(sockfd, MAX_CLIENTS) < 0)
+    {
+        std::cerr << "Blad w listen" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    return sockfd;
+}
+
+int main()
+{
+    server(8080);
+
+    return 0;
+}
+
+
